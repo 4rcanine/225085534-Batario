3ddf0621393b745f602a069781f0ef61460e40c1
4rcanine <145784416+4rcanine@users.noreply.github.com>
2024-11-07 09:50:40 +0800

    update

--- projects/wsp-two/update.php
+++ projects/wsp-two/update.php
@@ -7,15 +7,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_GET['id'])) {
     $product = $_POST['product'];
     $price = $_POST['price'];
 
-    // Prepare the update query
+   
     $sql = "UPDATE items SET product=?, price=? WHERE id=?";
     $stmt = $conn->prepare($sql);
 
     if ($stmt) {
-        // Bind parameters to the SQL query
+        
         $stmt->bind_param("sdi", $product, $price, $id);
         
-        // Execute the query
+       
         if ($stmt->execute()) {
             header("Location: index.php");
             exit();
